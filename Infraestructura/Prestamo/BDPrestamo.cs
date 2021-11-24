0e5e066df2ee765be517ffcb480789c1a37e54a0
Dierish05 <85861227+Dierish05@users.noreply.github.com>
2021-11-24 17:35:19 -0600

    Add files via upload

---
+++ Infraestructura/Prestamo/BDPrestamo.cs
@@ -0,0 +1,47 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Infraestructura.Prestamo
+{
+    public class BDPrestamo
+    {
+        public static List<Calendario> listaprestamos = new List<Calendario>();
+        public static List<Calendario> tmp = new List<Calendario>();
+
+        #region Catalogo 
+
+        public void Insertar(Calendario c)
+        {
+            listaprestamos.Add(c);
+        }
+
+
+        public void Eliminar(Calendario c)
+        {
+            listaprestamos.Remove(c);
+        }
+
+        public void Update(Calendario np, int id)
+        {
+            listaprestamos.Insert(id, np);
+
+        }
+
+        public Calendario[] GetAll()
+        {
+            if (listaprestamos == null)
+            {
+                throw new ArgumentException("No se han agregado datos aun.");
+            }
+
+            return listaprestamos.ToArray();
+        }
+
+
+        #endregion
+    }
+}
---
+++ Infraestructura/Prestamo/CalendarioCalcular.cs
@@ -0,0 +1,40 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infraestructura.Prestamo
+{
+    public class CalendarioCalcular : BDPrestamo
+    {
+        public decimal CalcularCuota(decimal principal, decimal interes)
+        {
+            return Math.Round(principal + interes);
+        }
+
+        public decimal CalcularInteres(decimal monto, decimal tasa, decimal terminos)
+        {
+            return Math.Round(monto * tasa / terminos);
+        }
+
+        public decimal CalcularPrincipal(decimal monto, decimal tasa, decimal terminos)
+        {
+            return Math.Round(monto * (tasa + 1) / terminos);
+        }
+
+        public List<Calendario> GetBy(Predicate<Calendario> predicate)
+        {
+            return listaprestamos.FindAll(predicate);
+        }
+
+        public int GetLastIndex()
+        {
+            if (listaprestamos.Count == 0)
+            {
+                return 0;
+            }
+
+            return listaprestamos.Count;
+        }
+    }
+}
